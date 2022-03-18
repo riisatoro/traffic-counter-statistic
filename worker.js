8cc7b31aed2aa06d05934c52fe75ad37b856dada
riisatoro <riisa.toro@gmail.com>
2022-03-18 19:14:41 +0200

    Initial commit

---
+++ worker.js
@@ -0,0 +1,12 @@
+const PAGE_STATUS = 'complete';
+
+const getVisitedPageURL = (tabId, { status }) => {
+  if (status !== PAGE_STATUS) return;
+  
+  chrome.tabs.get(tabId, function({id, url}){
+    console.log(id, url)    
+  });
+  
+}
+
+chrome.tabs.onUpdated.addListener(getVisitedPageURL)
