e450fb01ea8b9076fabe70f29052a36845c5c59a
riisatoro <riisa.toro@gmail.com>
2022-03-21 17:15:31 +0200

    Add base urls

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,3 @@
-import typing
-
 from fastapi import FastAPI
 
 
@@ -7,5 +5,37 @@ app = FastAPI()
 
 
 @app.get('/health')
-def health():
+def _health():
+    return 'ok'
+
+@app.post('/register')
+def _register():
+    return 'ok'
+
+@app.post('/login')
+def _login():
+    return 'ok'
+
+@app.post('/email/confirm')
+def _confirm_email():
+    return 'ok'
+
+@app.post('/password/reset')
+def _reset_password():
+    return 'ok'
+
+@app.post('/password/confirm')
+def _confirm_reset_password():
+    return 'ok'
+
+@app.get('/user/details')
+def _get_user_statistic():
+    return 'ok'
+
+@app.post('/user/details')
+def _set_user_details():
+    return 'ok'
+
+@app.get('/user/token')
+def _get_user_token():
     return 'ok'
